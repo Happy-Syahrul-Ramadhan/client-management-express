33f59c77ecdcec4509dce32fbd6d1b3c3379255d
EgiStr <egicuco50@gmail.com>
2023-05-08 10:50:52 +0700

    make route and admin/mentor/cleint dashboard

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,15 +2,22 @@ class HomeController {
   // index page
   indexPage(req, res, next) {
     if (!req.user.role) {
-    return res.render("pages/about");
+      return res.render("pages/about");
     }
     if (req.user.role === "mentor") {
       return res.render("pages/mentor");
     }
     return res.render("pages/index");
-    
   }
-
+  DashboardAdmin = async (req, res, next) => {
+    const userId = req.user.id;
+    try {
+      const user = await this.services.findById(userId);
+      console.log(user);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default HomeController;
